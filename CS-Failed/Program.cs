0a966f6e7610327fcc8c7fabc79f4d42e154038f
test@catlight.io <test@catlight.io>
2016-04-16 21:00:04 -0700

    Added build definition and projects

---
+++ CS-Failed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Failed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            break_the_build;
+        }
+    }
+}
---
+++ CS-Successful/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Successful
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
